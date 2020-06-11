72efe47578cfaa847872d9f74cb018a70644cb59
anasali2193 <anasali2193@gmail.com>
2020-06-11 21:08:42 +0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Header} from './components/Header';
 import {Balance} from './components/Balance';
+import {IncomeExpress} from './components/IncomeExpress';
+import {TransactionList} from './components/TransactionList';
+import {AddTransaction} from './components/AddTransaction';
+import {GlobalProvider} from './context/GlobalState'
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App"> 
+    <GlobalProvider className="App"> 
       <Header/>
       <div className='container'>
         <Balance/>
+        <IncomeExpress/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
         
-    </div>
+    </GlobalProvider>
   );
 }
 
